71f83cb4af67970b60a960002df5eef6c7346824
Danial Farhan <danialworsnop@gmail.com>
2018-02-06 15:40:28 +0800

    6/2/2018
    3.41 pm

--- Assets/Script/PassangerManager.cs
+++ Assets/Script/PassangerManager.cs
@@ -9,11 +9,15 @@ public class PassangerManager : MonoBehaviour {
 
     public GameObject Destination;
 
+    public GameObject Passanger;
+
     public void Awake()
     {
         guardIMG.SetActive(false);
 
         Destination.SetActive(false);
+
+        Passanger.SetActive(true);
     }
 
     public void OnTriggerEnter(Collider col)
@@ -22,7 +26,7 @@ public class PassangerManager : MonoBehaviour {
         {
             guardIMG.SetActive(true);
 
-            Debug.Log("Arep babi!");
+            Passanger.SetActive(false);
 
             Destroy(gameObject);
 
